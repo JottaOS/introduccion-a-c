d0ff6d0b911a86d38318e0bcd65aaee766be48cb
JottaOS <juanmendozasfa@gmail.com>
2024-07-19 22:55:42 -0400

    FIN.

--- tpFinal/tp.c
+++ tpFinal/tp.c
@@ -1,6 +1,6 @@
 #define GRNB "\e[42m"
 #define RED "\e[0;31m"
-#define BLK "\e[0;30m"
+#define BLK "\e[38;5;240m"
 #define COLOR_RESET "\e[0m"
 
 #include <stdio.h>
@@ -90,14 +90,12 @@ void mover_tablero_a_ordenado(Carta tablero[7][19], int col, int fila, Palo *maz
 {
     if (tablero[col][fila].valor != -1)
     {
-        ++(mazo_ordenado->tope);                                         // Incrementar tope para el nuevo elemento
-        mazo_ordenado->cartas[mazo_ordenado->tope] = tablero[col][fila]; // Asignar la carta
+        ++(mazo_ordenado->tope);
+        mazo_ordenado->cartas[mazo_ordenado->tope] = tablero[col][fila];
 
-        // Limpiar la posición del tablero
         tablero[col][fila].valor = -1;
-        tablero[col][fila].estado = 0; // O cualquier otro valor que denote un espacio vacío
+        tablero[col][fila].estado = 0;
 
-        // Si hay una carta debajo, hacerla visible
         if (fila > 0 && tablero[col][fila - 1].valor != -1)
         {
             tablero[col][fila - 1].oculto = 0;
@@ -115,7 +113,6 @@ void mover_tablero_a_tablero(Carta tablero[7][19], int col_origen, int fila_orig
         int i = fila_origen;
         int j = fila_destino;
 
-        // Mover todas las cartas desde fila_origen hacia abajo
         while (i < 19 && tablero[col_origen][i].valor != -1)
         {
             tablero[col_destino][j] = tablero[col_origen][i];
@@ -125,7 +122,6 @@ void mover_tablero_a_tablero(Carta tablero[7][19], int col_origen, int fila_orig
             j++;
         }
 
-        // Descubrir la carta que queda en la parte superior de la columna de origen, si existe
         if (fila_origen > 0 && tablero[col_origen][fila_origen - 1].valor != -1)
         {
             tablero[col_origen][fila_origen - 1].oculto = 0;
@@ -178,15 +174,15 @@ char letra_palo(int palo)
     switch (palo)
     {
     case 3:
-        return 'P'; // Picas
+        return 'P';
     case 4:
-        return 'C'; // Corazones
+        return 'C';
     case 5:
-        return 'T'; // Tréboles
+        return 'T';
     case 6:
-        return 'D'; // Diamantes
+        return 'D';
     default:
-        return '?'; // Valor desconocido
+        return '?';
     }
 }
 
@@ -268,22 +264,6 @@ void cargar_tablero(Carta Mazo_desordenado[52], Carta tablero[7][19])
             tablero[i][j] = carta_vacia;
         }
     }
-}
-
-void debug_mazo_desordenado(Carta mazo[52])
-{
-    int i;
-    printf("Mazo desordenado\n[ ");
-    for (i = 0; i < 52; i++)
-    {
-        if (mazo[i].estado != 2)
-        {
-
-            char *color = mazo[i].color == 0 ? RED : BLK;
-            printf("%s%s%s, ", color, mazo[i].impresion, COLOR_RESET);
-        }
-    }
-    printf("]\n");
 }
 
 void imprimir_tablero(Carta tablero[7][19], Carta mazo_desordenado[52], int indice_mazo, Palo palos[4])
@@ -451,18 +431,6 @@ InfoMovimiento obtener_info_movimiento(char *string, Carta tablero[7][19], Carta
         }
         return info;
     }
-    /* todo: corregir despues
-        int indice_palo =
-            primer_caracter == 'P'   ? 0
-            : primer_caracter == 'T' ? 1
-            : primer_caracter == 'R' ? 2
-            : primer_caracter == 'S' ? 3
-                                     : -1;
-
-        // todo: el tope se inicializa con -1, queda pendiente ver como se actualiza el tope al mover a un palo
-        return palos[indice_palo].cartas[palos->tope + 1];
-
-    */
 }
 
 void imprimir_accion(char string[])
@@ -609,47 +577,29 @@ void capturar(char string[])
 
 int validar_movimiento_palo(InfoMovimiento *movimiento, Palo palos[4], Carta tablero[7][19])
 {
-    // validar movimiento de palo
     int indice_palo = movimiento->destino[0];
     Palo palo = palos[indice_palo];
 
-    // validar si la carta está oculta
     if (movimiento->carta_origen.oculto == 1)
-    {
-        printf("La carta esta oculta. No se puede mover\n");
         return 0;
-    }
 
     if (movimiento->origen[1] != -1)
     {
         Carta carta_abajo = tablero[movimiento->origen[1]][movimiento->origen[0] + 1];
-
-        printf("Valor de la carta de abajo: %d\n", carta_abajo.valor);
         if (carta_abajo.valor != -1)
-        {
-            printf("No se puede mover la carta al palo porque tiene otras cartas debajo\n");
             return 0;
-        }
     }
 
-    // validar el símbolo si ya existen cartas en el palo
     if (palo.tope != -1)
     {
         Carta carta_tope;
         obtener_tope_palo(&carta_tope, palos, indice_palo);
         if (carta_tope.palo != movimiento->carta_origen.palo)
-        {
-            printf("Las cartas deben ser del mismo palo\n");
             return 0;
-        }
     }
 
-    // validar el valor de la carta
     if (palo.tope != movimiento->carta_origen.valor - 2)
-    {
-        printf("El valor del tope del palo debe ser %d pero es %d\n", movimiento->carta_origen.valor - 2, palo.tope);
         return 0;
-    }
 
     return 1;
 }
@@ -657,88 +607,38 @@ int validar_movimiento_palo(InfoMovimiento *movimiento, Palo palos[4], Carta tab
 int validar_movimiento_tablero(InfoMovimiento *movimiento, Carta tablero[7][19])
 {
 
-    // las posiciones son vectores de 2 elementos que representan la fila y la columna del tablero
     int fila_destino = movimiento->destino[0];
     int columna_destino = movimiento->destino[1];
 
     Carta carta_anterior = tablero[columna_destino][fila_destino - 1];
 
-    // validar que la carta no esté oculta
     if (movimiento->carta_origen.oculto)
-    {
-        printf("La carta esta oculta\n");
         return 0;
-    }
 
-    // no hay ninguna carta de origen
     if (movimiento->carta_origen.valor == -1)
-    {
-        printf("No hay una carta en la posicion de origen\n");
         return 0;
-    }
 
-    // que no reemplace una carta (la posicion de destino no está vacía)
     if (movimiento->carta_destino.valor != -1)
-    {
-        printf("Ya existe una carta en esa posicion\n");
         return 0;
-    }
 
-    // en caso de que la fila_destino sea 0, verificar que la carta que se pretende mover sea king
     if (fila_destino == 0)
     {
         if (movimiento->carta_origen.valor != 13)
-        {
-
-            printf("La carta debe ser King para mover a una columna vacia\n");
             return 0;
-        }
 
-        // ya no es necesario continuar con las validaciones de aquí en adelante, para este caso
         return 1;
     }
 
-    // verificar que en tablero[fila_destino - 1][columna_destino] exista una carta
     if (carta_anterior.valor == -1)
-    {
-        printf("El espacio previo al destino esta vacio\n");
         return 0;
-    }
 
-    // la carta debe ser del color opuesto (si mi carta es roja, la otra debe ser negra y viceversa)
-    // valores de colores: rojo = 0, negro = 1
     if (carta_anterior.color == movimiento->carta_origen.color)
-    {
-        printf("Ambos colores son iguales\n");
         return 0;
-    }
 
-    // el valor de la carta en la posicion tablero[fila_destino - 1][columna_destino] debe tener valor de nuestra carta de origen + 1.
     if (carta_anterior.valor != movimiento->carta_origen.valor + 1)
-    {
-        printf("El valor de la carta anterior debe ser igual a %d pero es %d\n", movimiento->carta_origen.valor + 1, carta_anterior.valor);
         return 0;
-    }
 
     return 1;
-}
-// todo: borrar, esto es solo para debug
-void imprimirInfoMovimiento(const InfoMovimiento *movimiento)
-{
-    printf("** DEBUG **\n");
-    // Imprimir información de la carta
-    printf("Carta origen: ");
-    imprimir_carta(movimiento->carta_origen);
-    printf("\n");
-    printf("Carta destino: ");
-    imprimir_carta(movimiento->carta_destino);
-    printf("\n");
-    // Imprimir tipo de movimiento
-    printf("Tipo de movimiento: %c\n", movimiento->tipo_movimiento);
-    // Imprimir origen
-    printf("Origen: [%d, %d]\n", movimiento->origen[0], movimiento->origen[1]);
-    // Imprimir destino
-    printf("Destino: [%d, %d]\n", movimiento->destino[0], movimiento->destino[1]);
 }
 
 void imprimir_carta_log(FILE *f, Carta carta)
@@ -811,12 +711,13 @@ bool verificar_victoria(Palo palos[4])
     for (i = 0; i < 4; i++)
     {
         if (palos[i].tope != 12)
-        {                 // 13 cartas, índice de la última carta es 12
-            return false; // Si cualquier palo no está lleno, no hay victoria
+        {
+            return false;
         }
     }
-    return true; // Todos los palos están llenos
+    return true;
 }
+
 int juego(Carta Mazo[52], Carta Mazo_desordenado[52], Carta tablero[7][19], Palo palos[4])
 {
     char string[8];
@@ -832,7 +733,6 @@ int juego(Carta Mazo[52], Carta Mazo_desordenado[52], Carta tablero[7][19], Palo
     imprimir_tablero(tablero, Mazo_desordenado, indice_mazo, palos);
     while (!juego_finalizado)
     {
-        debug_mazo_desordenado(Mazo_desordenado);
         nro_jugada++;
         escribir_log(tablero, Mazo_desordenado, indice_mazo, nro_jugada, palos);
         capturar(string);
@@ -868,7 +768,7 @@ int juego(Carta Mazo[52], Carta Mazo_desordenado[52], Carta tablero[7][19], Palo
                 if (indice_mazo + 1 >= 52)
                 {
                     printf("Reiniciando indice mazo");
-                    indice_mazo = 0; // Resetear el índice del mazo
+                    indice_mazo = 0;
                 }
                 indice_mazo++;
             } while (Mazo_desordenado[indice_mazo].estado == 2);
@@ -876,7 +776,6 @@ int juego(Carta Mazo[52], Carta Mazo_desordenado[52], Carta tablero[7][19], Palo
         else
         {
             InfoMovimiento info_movimiento = obtener_info_movimiento(string, tablero, Mazo_desordenado, palos, indice_mazo);
-            imprimirInfoMovimiento(&info_movimiento); // todo: esta funcion es solo de debug y se borra antes de entregar
             if (info_movimiento.tipo_movimiento == 'T')
             {
                 if (!validar_movimiento_tablero(&info_movimiento, tablero))
@@ -955,25 +854,6 @@ int juego(Carta Mazo[52], Carta Mazo_desordenado[52], Carta tablero[7][19], Palo
         }
         printf("\n");
         imprimir_tablero(tablero, Mazo_desordenado, indice_mazo, palos);
-    }
-}
-
-// todo: borrar pq es para PRUEBA de CONDICION DE VICTORIA
-void inicializar_palos_para_prueba(Palo palos[4])
-{
-    int i, j;
-    for (i = 0; i < 4; i++)
-    {
-        palos[i].tope = 12; // Indica que hay 13 cartas (de 0 a 12)
-        for (j = 0; j < 13; j++)
-        {
-            palos[i].cartas[j].valor = j + 1;
-            palos[i].cartas[j].palo = i + 3; // Asignar un palo diferente a cada uno (P = 3, T = 4, R = 5, S = 6)
-            palos[i].cartas[j].estado = 2;
-            palos[i].cartas[j].oculto = 0;
-            palos[i].cartas[j].color = (i < 2) ? 1 : 0;                    // Asignar colores (negro para P y T, rojo para R y S)
-            sprintf(palos[i].cartas[j].impresion, "%d%c", j + 1, 'P' + i); // P, T, R, S
-        }
     }
 }
 
